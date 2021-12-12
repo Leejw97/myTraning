e89bb89964b2fe314ecdf29b62b0a2b31be2af11
Leejw97 <88294755+Leejw97@users.noreply.github.com>
2021-12-13 00:35:25 +0900

    ss

---
+++ PP/JS/media.js
@@ -0,0 +1,14 @@
+function showMenu(){
+
+  // 함수호출확인
+  // alert("나야나");
+
+  // 1.변경대상 선정 : .gnb
+  // 선택요소를 gnb 라는 이름의 변수에 담는다
+  var gnb = 
+  document.getElementsByClassName("gnb").item(0);
+  // 전체문서.가져와클래스(클래스명).몇번째(0)
+
+  // 2. 변경내용 : 대상에게 클래스 "on"넣거나 빼기
+  gnb.classList.toggle("on");
+}
