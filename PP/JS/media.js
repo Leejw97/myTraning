e8d4805168d8800433d5a4129be8b9b4bd14f179
Leejw97 <88294755+Leejw97@users.noreply.github.com>
2021-12-15 18:28:51 +0900

    ss

--- PP/JS/media.js
+++ PP/JS/media.js
@@ -2,10 +2,13 @@
 
 // });
 
+// 햄버거 버튼 나왔다가 안나왔다가 할때
+// let hsts=1;//햄버거상태(0-없음,1-있음)
+
 function showMenu(){
 
-  // 함수호출확인
-  // alert("나야나");
+  var ham =
+  document.querySelector(".ham");
 
   // 1.변경대상 선정 : .gnb
   // 선택요소를 gnb 라는 이름의 변수에 담는다
@@ -15,4 +18,15 @@ function showMenu(){
 
   // 2. 변경내용 : 대상에게 클래스 "on"넣거나 빼기
   gnb.classList.toggle("on");
+
+
+  // 햄버거 버튼 나왔다가 안나왔다가 할때
+  // if(hsts)
+  // ham.style.display = "none";
+  // else
+  // ham.style.display = "block";
+
+  // // hsts상태를 반대로 만들기!
+  // hsts===1? hsts =  0 : hsts = 1;
 }
+
